d73c19182a547772300a2371ec1c7093fb6198d1
suvas <ss5138275@gmail.com>
2026-09-06 15:38:59 +0545

    build:screen navigation of confirm

--- app/index.js
+++ app/index.js
@@ -1,19 +1,26 @@
 import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "../screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
-
+import { useState } from "react";
+import GameScreen from "../screens/GameScreen";
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
   return (
     <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
       <ImageBackground
         source={require("../assets/background/dice.jpg")}
         resizeMode="cover"
         style={styles.rootScreen}
-        imageStyle={
-          styles.backgroundImage
-        }
+        imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
@@ -23,7 +30,7 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
   },
-  backgroundImage:{
-    opacity:0.15
-  }
+  backgroundImage: {
+    opacity: 0.15,
+  },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -22,7 +22,7 @@ function StartGameScreen() {
       ]);
       return;
     }
-    console.log("Valid Number");
+    onPickNumber(chosenNumber);
   }
   return (
     <View style={styles.rootContainer}>
